c1e93e05096c36be5317aa80707089ed0194adf7
hitoshitakenuki <reportfrombamboo@gmail.com>
2021-10-17 15:36:38 +0900

    db

--- index.js
+++ index.js
@@ -2,21 +2,32 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express();
-const { Client } = require('pg');
-var client = new Client({
-  user: 'bxdhrqzeozmuws', 
-  host: 'ec2-18-207-72-235.compute-1.amazonaws.com',
-  database: 'd2umj76u4q9ods',
-  password: '021af8e3cc3e2d38f94982afedd7741c6214e93fbb09d0400e1d8373120b233e', 
-  post: 5432
-})
 
-client.connect();
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('pages/top'));
+
 app.get('/index', (req, res) => res.render('pages/index'));
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
