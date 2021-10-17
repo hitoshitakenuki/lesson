3fea01bb458d18391088d8e0ed0855a0ac9926c7
hitoshitakenuki <reportfrombamboo@gmail.com>
2021-10-17 14:39:02 +0900

    psgr

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express();
 const { Client } = require('pg');
+var client = new Client({
+  user: 'bxdhrqzeozmuws', 
+  host: 'ec2-18-207-72-235.compute-1.amazonaws.com',
+  database: 'd2umj76u4q9ods',
+  password: '021af8e3cc3e2d38f94982afedd7741c6214e93fbb09d0400e1d8373120b233e', 
+  post: 5432
+})
 
+client.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
